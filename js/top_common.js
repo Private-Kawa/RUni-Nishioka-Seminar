a34a2242ed520a41945ab97d59e9ca8ebf6e318c
taigakatoo <pluswisteriga35@gmail.com>
2023-10-31 12:24:27 +0900

    topヘッダーフッター各リンク修正

--- js/top_common.js
+++ js/top_common.js
@@ -1,4 +1,4 @@
 $(function () {
-	$(".header").load("common/header.html");
-	$(".footer").load("common/footer.html");
+	$(".header").load("common/top_header.html");
+	$(".footer").load("common/top_footer.html");
 });
